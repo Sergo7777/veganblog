48dd53399045cc795fa6eb91beedd71d45dc8730
Sergo7777 <serega_denisenko@mail.ru>
2017-07-30 22:44:02 +0300

    heroku

--- posts/forms.py
+++ posts/forms.py
@@ -8,6 +8,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = [
             'title', 
+            'slug',
             'image',
             'content',
             'draft',
