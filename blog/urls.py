1bbf2cc5bcf03231643b79b68a550fb794c251e6
Sergo7777 <serega_denisenko@mail.ru>
2017-07-30 15:58:44 +0300

    heroku

--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls", namespace='posts'),),
+    url(r'^', include("posts.urls", namespace='posts'),),
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
